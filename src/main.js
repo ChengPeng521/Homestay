a6132517c9708449591080ffae7bce3412d344b0
ChengPeng <2410208224@qq.com>
2024-07-01 14:34:03 +0800

    day1

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import axios from 'axios';
+axios.defaults.baseURL= "http://localhost:9000"
+Vue.prototype.path= "http://localhost:9000"
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
