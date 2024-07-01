024302ba1c40cac74c859f44e78c6af4eef2413a
ChengPeng <2410208224@qq.com>
2024-07-01 14:33:49 +0800

    day1

---
+++ src/views/test.vue
@@ -0,0 +1,20 @@
+<template>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {}
+};
+</script>
+<style  scoped>
+</style>
