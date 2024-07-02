4ea1217748d8890e1d7c5f8899c8b526d6e02a2f
ChengPeng <2410208224@qq.com>
2024-07-02 09:07:24 +0800

    init

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 
 import axios from 'axios';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+
 axios.defaults.baseURL= "http://localhost:9000"
 Vue.prototype.path= "http://localhost:9000"
 Vue.prototype.$http = axios
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,6 +1,59 @@
 <template>
-    <div>
-        <h1>登录界面</h1>
+    <div class="login-container">
+        <el-container>
+            <el-header class="login-header">
+                <div>
+                    <img src="../assets/etoak.png" width="200" height="50">
+                </div>
+                <div>
+                    <span>首页</span>
+                </div>
+            </el-header>
+            
+            <el-main class="login-main">
+                <el-card>
+                    <el-tabs v-model="activeName">
+                        <el-tab-pane label="账号密码登录" name="first">
+
+                            <el-form :model="loginForm" ref="loginForm" :rules="loginRules">
+
+                                <el-form-item prop="username">
+                                    <el-input v-model="loginForm.username" placeholder="请输入账号"></el-input>
+                                </el-form-item>
+
+                                <el-form-item prop="password">
+                                    <el-input v-model="loginForm.password" placeholder="请输入密码" show-password></el-input>
+                                </el-form-item>
+
+                                <el-form-item>
+                                    <el-button type="warning" style="width: 100%;" @click="login">登录</el-button>
+                                </el-form-item>
+
+                            </el-form>
+
+                        </el-tab-pane>
+
+                        <el-tab-pane label="邮箱登录" name="second">
+                            <el-form :model="emailForm" ref="emailForm">
+
+                                <el-form-item>
+                                    <el-input v-model="emailForm.email" placeholder="请输入邮箱"></el-input>
+                                </el-form-item>
+
+                                <el-form-item>
+                                    <el-input v-model="emailForm.code" placeholder="请输入验证码"></el-input>
+                                </el-form-item>
+
+                                <el-form-item>
+                                    <el-button type="warning" style="width: 100%;" @click="emailLogin">登录</el-button>
+                                </el-form-item>
+
+                            </el-form>
+                        </el-tab-pane>
+                    </el-tabs>
+                </el-card>
+            </el-main>
+        </el-container>
     </div>
 </template>
 
@@ -10,14 +63,69 @@
         props: {},
         data() {
             return {
+                activeName:'first',
+                loginForm: {
+                    username: null,
+                    password: null,
+                },
+                emailForm: {
+                    email: null,
+                    code: null,
+                },
+                loginRules: {
+                    username: [
+                        {required: true,message:'请输入账号',trigger:'blur'}
+                    ],
+                    password: [
+                        {required: true,message:'请输入密码',trigger:'blur'}
+                    ],
+                }
             };
         },
-        watch: {},  
+        watch: {},
         computed: {},
-        methods: {},
+        methods: {
+            login() {
+                this.$message({
+                    message: '恭喜您,登录成功',
+                    type: 'success'
+                });
+                // 与上面等价
+                // this.$message.success(恭喜您,登录成功)
+            },
+            emailLogin() {
+                this.$message.success("恭喜您,登录成功")
+            },
+            
+
+        },
         created() {},
         mounted() {}
     };
 </script>
 <style  scoped>
+    .login-container{
+        height: 100vh;
+    }
+    .login-header{
+        height: 10vh;
+        display:flex;      
+        justify-content: space-around;
+        align-items: center;
+        margin-top: 20px;
+    }
+    .login-main{
+        height: 90vh;
+        background: url('../assets/background.jpg') no-repeat;
+        background-position: center;
+        background-size: cover;
+        
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .login-main .el-card {
+        width: 400px;
+        padding: 20px;
+    }
 </style>
